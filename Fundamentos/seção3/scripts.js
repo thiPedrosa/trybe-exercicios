739c8e8e9d4ac8b8e5004c0a40af00edac6d68f2
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-02 16:17:20 -0300

    Para fixar &&

--- Fundamentos/seção3/scripts.js
+++ Fundamentos/seção3/scripts.js
@@ -1,10 +1,15 @@
-let nota = 40;
-
-if (nota >= 80) {
-  console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");  
-} else if (nota <= 80 && nota >= 60){ 
-  console.log("Você está na nossa lista de espera.");
-}else{
-  console.log("Infelizmente você reprovou.");
+let currentHour = 3
+let message;
+if (currentHour >= 22) {
+  message = "Não deveríamos comer nada, é hora de dormir"
+}else if (currentHour >= 18 && currentHour <= 22){
+  message = "Rango da noite, vamos jantar :D"
+}else if (currentHour >= 14 && currentHour <= 18){
+  message = "Vamos fazer um bolo pro café da tarde?"
+}else if (currentHour >= 11 && currentHour <= 14){
+  message = "Hora do almoço!!!"
+}else if (currentHour>= 4 && currentHour <= 11){
+  message = "Hmmm, cheiro de café recém-passado"
 }
 
+console.log(message);
