43b8df61c056dda0cd6d04e66980c03774ca99cd
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-02 20:54:19 -0300

    Corrige texto

--- Fundamentos/seção3/dia-1-JavaScript-Primeiros-Passos/scripts.js
+++ Fundamentos/seção3/dia-1-JavaScript-Primeiros-Passos/scripts.js
@@ -1,5 +1,5 @@
 //Exercícios na pratica
-//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+//1 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
 
 let a = 5;
 let b = 10;
