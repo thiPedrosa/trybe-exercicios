7562a030b30e3512cfec6431bab05b8944fcdba1
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-02 18:27:10 -0300

    Para Fixar SwitchCase

--- Fundamentos/seção3/scripts.js
+++ Fundamentos/seção3/scripts.js
@@ -1,7 +1,17 @@
-let weekDay = "Domingo"
+let status = ""
 
-if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay ==='quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira'){
-  console.log('Oba, mais um dia de aprendizado na Trybe >:D');
-} else {
-  console.log("FINALMENTE, descanso merecido! UwU!");
+switch(status) {
+  case "aprovada":
+    console.log("Parabéns, você está no grupo de pessoas aprovadas!")
+    break
+  case "lista":
+    console.log("Você está na nossa lista de espera.")
+    break
+
+  case "reprovada":
+    console.log("Infelizmente, você reprovou.")
+    break
+  
+  default:
+    console.log("Informação incorreta.")
 }
