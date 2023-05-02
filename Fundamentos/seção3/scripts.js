26c95cdb7bd1c8bf0ba6d42f9d9c11f9f7d471ef
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-02 16:05:42 -0300

    Para fixar foguete 3

--- Fundamentos/seção3/scripts.js
+++ Fundamentos/seção3/scripts.js
@@ -1,10 +1,10 @@
-let base = 5
-let height = 8
-let area = base * height
-let perimeter = (base*2) + (height*2)
+let nota = 40;
 
-console.log(base);
-console.log(height);
-console.log(area);
-console.log(perimeter);
+if (nota >= 80) {
+  console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");  
+} else if (nota <= 80 && nota >= 60){ 
+  console.log("Você está na nossa lista de espera.");
+}else{
+  console.log("Infelizmente você reprovou.");
+}
 
