84399eaa52f776e6c0703f54acd0a7a7f310db89
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-08 15:35:58 -0300

    Resolve Adicionando novas Chaves

--- Fundamentos/seção3/dia-4-JavaScript-Primeiros-Passos/script.js
+++ Fundamentos/seção3/dia-4-JavaScript-Primeiros-Passos/script.js
@@ -25,3 +25,36 @@ let car = {
 for (key in car){
   console.log(key + ': ' + car[key]);
 }
+
+//Exercicio - Adicionando chaves:
+
+// Após ter conhecido diferentes maneiras de associar chaves e valores a um objeto, que tal praticar? Adicione suas informações ao objeto student = {}, como nome, e-mail, telefone, GitHub e LinkedIn, usando os métodos abordados no conteúdo.
+
+let student = {};
+
+console.log(student);
+
+
+let newName = 'name';
+let name = 'Thiago'
+let newEmail = 'email';
+let email = 'thiagopedrosaw@gmail.com';
+let newPhone =  'telefone';
+let telefone = 41991829351;
+let newGitHub = 'gitHub';
+let gitHub = '';
+let newLinkedin = 'linkedIn';
+let linkedIn = '';
+
+function addProperty (object, key, value){
+  object[key] = value
+}
+
+addProperty(student, newName, name);
+addProperty(student, newEmail, email);
+addProperty(student, newPhone, telefone);
+addProperty(student, newGitHub, gitHub);
+addProperty(student, newLinkedin, linkedIn);
+
+
+console.log(student);
