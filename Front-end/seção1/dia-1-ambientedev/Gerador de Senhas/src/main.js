d74b58e17b3e034edc623cfc35b400b508805d28
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-06-21 21:04:20 -0300

    install copy, add alert, copy password

--- Front-end/seção1/dia-1-ambientedev/Gerador de Senhas/src/main.js
+++ Front-end/seção1/dia-1-ambientedev/Gerador de Senhas/src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const randomPassword = nanoid();
 console.log(randomPassword);
@@ -8,4 +9,9 @@ const displayPasswordEL = document.querySelector('h2');
 
 passwordBtn.addEventListener('click', () => {
   displayPasswordEL.innerHTML = nanoid();
+});
+displayPasswordEL.addEventListener('click', () => {
+  copy(displayPasswordEL.innerHTML);
+  // eslint-disable-next-line no-alert
+  alert('Senha copiada!');
 });
